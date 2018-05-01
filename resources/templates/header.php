a0c489484525a68dbb50855d9883877cfdfa482a
Lea Rottensten Lamp <leax1777@edu.campusvejle.dk>
2018-05-01 11:30:52 +0200

    Changes file paths in header.php

--- resources/templates/header.php
+++ resources/templates/header.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" href="resources/images/favicon.png">
-    <link rel="stylesheet" href="resources/css/master.min.css">
+    <link rel="stylesheet" href="./resources/css/master.min.css">
     <?php if(isset($css)) {
         // If page has an individual css file, link to it
-        ?> <link rel="stylesheet" href=<?php echo "resources/css/" . $css . ".min.css" ?>> <?php
+        ?> <link rel="stylesheet" href=<?php echo "./resources/css/" . $css . ".min.css" ?>> <?php
     } ?>
     <?php if (isset($pageTitle)) {
         // If page title is set, add on the page title
