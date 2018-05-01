6959a0a5b068dd3bedd0fccba78acd5165cfc14c
Linus Bruno Hagström Brosbøl <linu0043@edu.campusvejle.dk>
2018-05-01 11:20:00 +0200

    Added favicon and made logo SVG

--- resources/templates/header.php
+++ resources/templates/header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="resources/images/favicon.png">
     <link rel="stylesheet" href="resources/css/master.min.css">
     <?php if(isset($css)) {
         // If page has an individual css file, link to it
